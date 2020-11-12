483f92a8c6a7ee1d574c330a1ec8a7fca33fa488
me@pc <tHoMaStHeRmOnUcLeArBoMb@localhost>
2020-11-12 22:23:06 +0100

    added boilerplate for server

---
+++ sidetracked-server/api.py
@@ -0,0 +1,15 @@
+def upload_files(json_data):
+    # do the things needed
+    return 200
+
+def download_files(json_data):
+    # do the things needed
+    return 200
+
+def list_files(json_data):
+    # do the things needed
+    return 200
+
+def hearbeat(json_data):
+    # do the things needed
+    return 200
---
+++ sidetracked-server/server.py
@@ -0,0 +1,48 @@
+from flask import Flask, render_template, request
+from flask_restful import Resource, Api
+
+
+from api import hearbeat
+from api import list_files
+from api import upload_files
+from api import download_files
+
+
+app = Flask(__name__)
+api = Api(app)
+
+class index(Resource):
+    def get(self):
+        return render_template("index.html")
+
+# returning request.get_json is just for lack of anything else and will not be the case in the end
+
+class api_files(Resource):
+    def get(self):
+        response = download_files(request.get_json)
+        return request.get_json(), response
+    def post(self):
+        response = upload_files(request.get_json)
+        return request.get_json(), response
+
+
+class api_ls(Resource):
+    def get(self):
+        response = list_files(request.get_json)
+        return request.get_json(), response
+
+
+class api_heartbeat(Resource):
+    def get(self):
+        response = hearbeat(request.get_json)
+        return request.get_json(), response
+
+
+api.add_resource(index, '/')
+api.add_resource(api_ls, '/api/v0/ls/')
+api.add_resource(api_files, '/api/v0/file/')
+api.add_resource(api_heartbeat, '/api/v0/hearbeat/')
+
+
+if __name__ == "__main__":
+    app.run(host='localhost', port=5000, debug=True)
