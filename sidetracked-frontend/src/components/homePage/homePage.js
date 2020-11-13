88093070d323388e9ef97fd2183d896f4131d377
Steven Dellamore <dellamoresteven@gmail.com>
2020-11-13 12:29:58 -0600

    hello

---
+++ sidetracked-frontend/src/components/homePage/homePage.js
@@ -0,0 +1,19 @@
+
+import React, { Component } from 'react';
+
+class HomePage extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        console.log("HERE")
+        return (
+            <div>
+                Hellooooooooooooooooooooooooo
+            </div>
+        );
+    }
+}
+
+export default HomePage;
