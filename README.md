440b3d3841cf6afadd4faf27fb80deafabb1527f
Steven Dellamore <dellamoresteven@gmail.com>
2020-11-13 11:22:33 -0600

    boilerplate react stuff

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ Fully modular collaboration platform
 # How to start
 1. `cd sidetracked-frontend`
 2. `npm i`
-3. `npm start`
+3. `npm run dev`
--- sidetracked-frontend/src/index.js
+++ sidetracked-frontend/src/index.js
@@ -1,31 +1,9 @@
-const { app, BrowserWindow } = require('electron');
-const path = require('path');
-const homePage = require('./homePage/homePage');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
 
-
-// Handle creating/removing shortcuts on Windows when installing/uninstalling.
-if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
-  app.quit();
-}
-
-app.on('ready', homePage.createHomePage);
-
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
-app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
-});
-
-app.on('activate', () => {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow();
-  }
-});
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and import them here.
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
