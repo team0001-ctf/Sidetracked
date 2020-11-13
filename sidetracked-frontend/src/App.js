a355121f8079ab25fdf3ccad32af5a5916cd9845
Steven Dellamore <dellamoresteven@gmail.com>
2020-11-13 12:30:24 -0600

    someting else

--- sidetracked-frontend/src/App.js
+++ sidetracked-frontend/src/App.js
@@ -1,18 +1,15 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import HomePage from './components/homePage/homePage';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React/Electron</h2>
-        </div>
-        <p className="App-intro">
-          Hello Electron!
-        </p>
+				<div style={{  height: "100vh", width: "100%", backgroundColor:"#c1c1c1"}}>
+          <HomePage />
+				</div>
       </div>
     );
   }
