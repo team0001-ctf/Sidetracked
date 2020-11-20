30c8f40db5adfcc68fccb633a0ec2fc4bed24f4c
Steven Dellamore <dellamoresteven@gmail.com>
2020-11-20 12:11:56 -0600

    python code

--- sidetracked-frontend/src/components/editors/editor.js
+++ sidetracked-frontend/src/components/editors/editor.js
@@ -36,7 +36,8 @@ class Markdown extends React.Component {
         this.state = {
             code: "# UNABLE TO LOAD RIP",
             file_name: props.location.file_name,
-            file_type: type
+            file_type: type,
+            exe_out: ""
         }
         
     }
@@ -73,16 +74,47 @@ class Markdown extends React.Component {
             var res_json = JSON.parse(body);
             // let decoded_file = atob(res_json.data)
             console.log(res_json)
-            // if(decoded_file != this.state.code) {
-            //     this.setState({ 
-            //         code: decoded_file
-            //     })
-            // }
+            if(res_json["out"] != this.state.exe_out) {
+                this.setState({ 
+                    exe_out: res_json["out"]
+                })
+            }
         })
+    }
+
+    renderExeOut() {
+        if(this.state.exe_out != "") {
+            return (
+                <AceEditor
+                    placeholder="Start Editing"
+                    mode="text"
+                    theme="chrome"
+                    name="blah2"
+                    height="20vh"
+                    width="100%"
+                    fontSize={20}
+                    showPrintMargin={true}
+                    showGutter={true}
+                    highlightActiveLine={true}
+                    value={this.state.exe_out}
+                    wrapEnabled={true}
+                    setOptions={{
+                        enableBasicAutocompletion: true,
+                        enableLiveAutocompletion: true,
+                        enableSnippets: false,
+                        showLineNumbers: true,
+                        tabSize: 4,
+                    }}
+                />
+            )
+        } else {
+            return
+        }
     }
 
     // https://github.com/securingsincity/react-ace/blob/90255338bdb4312db6a2caf81decfb153b401a1e/docs/Ace.md
     render() {
+        var first_ace_size = this.state.exe_out != "" ? "75vh" : "95vh"
         return (
             <div style={{  height: "100vh", width: "100vw", backgroundColor:"#1e2227" }}>
                 <Navbar bg="primary" variant="dark" style={{  height: "5vh" }}>
@@ -97,7 +129,7 @@ class Markdown extends React.Component {
                     mode={this.state.file_type}
                     theme="chrome"
                     name="blah2"
-                    height="95vh"
+                    height={first_ace_size}
                     width="100%"
                     // onLoad={this.onLoad}
                     onChange={(newCode) => {
@@ -118,6 +150,7 @@ class Markdown extends React.Component {
                         tabSize: 4,
                     }}
                 />
+                {this.renderExeOut()}
             </div>
 
         )
