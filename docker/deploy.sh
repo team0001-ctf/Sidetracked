f33f7b75ce6c4ea0b353ddce69c1ee45c1d621ed
thomasthethermonuclearbomb <morrissbestyen@gmail.com>
2021-04-15 10:25:19 +0200

    added nginx start

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -1,5 +1,19 @@
 cd ../
 
+function runc() {
+    echo "root_script running: $@"
+    $@
+    if [ "$?" != 0 ]
+    then
+        killall python3
+    fi
+}
+
+#nginx
+runc systemctl enable nginx
+runc nginx -t
+runc systemctl start nginx
+
 # python3 -u main.py
 gunicorn --bind 0.0.0.0:5000 --log-level=debug server:app
 
